696927234d6732e1d9e51e0cb2fff55fb3ed7c3a
Gaurav-Sanchania <gauravinfinnium@gmail.com>
2025-03-31 10:04:01 +0530

    _ removed .vs directory

--- Infinnium_Website.Server/obj/Debug/net8.0/Infinnium_Website.Server.AssemblyInfo.cs
+++ Infinnium_Website.Server/obj/Debug/net8.0/Infinnium_Website.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infinnium_Website.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58513ff6e8bf7b922de12b4ef9fbb797a9710fef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infinnium_Website.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infinnium_Website.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
