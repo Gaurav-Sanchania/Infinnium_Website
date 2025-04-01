ebd3bdf76daa020b6dc07b09d26d0988bd478482
Gaurav-Sanchania <gauravinfinnium@gmail.com>
2025-04-01 09:24:43 +0530

    Home Page Layout Created

--- infinnium_website.client/src/components/home/home-layout/home-layout.component.ts
+++ infinnium_website.client/src/components/home/home-layout/home-layout.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
+import { WhatWeDoComponent } from '../what-we-do/what-we-do.component';
+import { AreaOfExpertiseComponent } from '../area-of-expertise/area-of-expertise.component';
+import { OurProductsComponent } from '../our-products/our-products.component';
+import { AboutUsComponent } from '../../../shared/components/about-us/about-us.component';
+import { GlobalImpactComponent } from '../global-impact/global-impact.component';
+import { TrustedLeadersComponent } from '../../../shared/components/trusted-leaders/trusted-leaders.component';
+import { RecentBlogsComponent } from '../../../shared/components/recent-blogs/recent-blogs.component';
+import { ExpertSectionComponent } from '../../../shared/components/expert-section/expert-section.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-home-layout',
-  imports: [],
+  imports: [HeaderComponent, HeroSectionComponent, WhatWeDoComponent, AreaOfExpertiseComponent,
+    OurProductsComponent, AboutUsComponent, GlobalImpactComponent, TrustedLeadersComponent,
+    RecentBlogsComponent, ExpertSectionComponent, FooterComponent],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.css'
 })
