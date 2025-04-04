92f42b0e8cee22dcd449d0c61d49e7d15327c3ea
Gaurav-Sanchania <gauravinfinnium@gmail.com>
2025-04-04 12:36:08 +0530

    Scroll Top bug - fixed

--- infinnium_website.client/src/main.ts
+++ infinnium_website.client/src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './routing/routes';
 
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),],
 }).catch(err => console.error(err));
