aa78545d30bdddd43a56d1adfa3baa07d5596880
Gaurav-Sanchania <gauravinfinnium@gmail.com>
2025-04-08 18:52:38 +0530

    Members list dynamic - AboutUs page

--- infinnium_website.client/src/auth/About Us/team/team.component.ts
+++ infinnium_website.client/src/auth/About Us/team/team.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthorService } from '../../../services/authorService.service';
 
 @Component({
   standalone: true,
   selector: 'app-team',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
+  providers: [AuthorService],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
-export class TeamComponent {
+export class TeamComponent implements OnInit {
+  constructor(private authorService: AuthorService) { }
 
+  public authors: any = [];
+
+  async ngOnInit() {
+    this.authors = await this.authorService.getAllAuthors();
+  }
 }
---
+++ infinnium_website.client/src/services/authorService.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { firstValueFrom } from "rxjs/internal/firstValueFrom";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthorService {
+  constructor(private httpClient: HttpClient) { }
+
+  async getAllAuthors(): Promise<{ id: number; name: string; description: string; email: string; designation: string; guid: string; }[]> {
+    try {
+      const response = await firstValueFrom(
+        this.httpClient.get<{ id: number; name: string; description: string; email: string; designation: string; guid: string; }[]>
+          (`https://localhost:7046/AuthorController/GetAllAuthors`));
+      return response;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  async getAuthorDetails(guid: string): Promise<{ id: number; name: string; description: string; email: string; designation: string; guid: string; }[]> {
+    try {
+      const response = await firstValueFrom(
+        this.httpClient.post<{ id: number; name: string; description: string; email: string; designation: string; guid: string; }[]>
+          (`https://localhost:7046/AuthorController/AuthorDetails/${guid}`, guid));
+      return response;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+}
