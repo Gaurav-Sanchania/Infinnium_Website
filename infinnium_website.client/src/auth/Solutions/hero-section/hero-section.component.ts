dbc1119731369ea4fa48a710c1473fd1257b9bbd
Gaurav-Sanchania <gauravinfinnium@gmail.com>
2025-04-14 16:20:00 +0530

    Solutions Hero section - Dynamic

--- infinnium_website.client/src/auth/Solutions/hero-section/hero-section.component.ts
+++ infinnium_website.client/src/auth/Solutions/hero-section/hero-section.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-solutions-hero-section',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
 export class HeroSectionComponent {
-
+  @Input() solution = "";
 }
