bd85ece7ddf179590e5a0b8e6ea08f77bc51b4a1
JayPurohit318 <purohitjay07@gmail.com>
2025-05-01 11:43:18 +0530

    made an animation for prosuct hero section breach profiler and contact us and monior changes of spacing

--- infinnium_website.client/src/auth/Contact Us/reach-out/reach-out.component.ts
+++ infinnium_website.client/src/auth/Contact Us/reach-out/reach-out.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-reach-out',
-  imports: [],
   templateUrl: './reach-out.component.html',
   styleUrl: './reach-out.component.css',
 })
-export class ReachOutComponent {}
+export class ReachOutComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries, obs) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const el = entry.target as HTMLElement;
+            el.classList.add('show');
+            obs.unobserve(el);
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    const animatedElements = document.querySelectorAll('.fade-up');
+    animatedElements.forEach(el => observer.observe(el));
+  }
+}
